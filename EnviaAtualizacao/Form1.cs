396a64d01aa460b5a8cfe3e010b44b147b470371
Xeviousbr <xeviousbr@gmail.com>
2024-04-23 21:23:58 -0300

    Anotações

--- EnviaAtualizacao/Form1.cs
+++ EnviaAtualizacao/Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Ler INI para ver onde esta o arquivo a ser enviado
+// Ler a versão do executável
+// Escrever um arquivo versao.txt com a versão e o texto
+// Ler no INI o caminho a ser enviado
+// Enviar o executável e a versão para o FTP
+// Atualizar o Progress
+// Encerrar o programa
+
 namespace EnviaAtualizacao
 {
     public partial class Form1 : Form
