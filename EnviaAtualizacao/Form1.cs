e7e4a708f27cb6aa91e04df29024341ef3d5d76c
Xeviousbr <xeviousbr@gmail.com>
2024-04-26 02:21:39 -0300

    Pega a versão do executável e mostra

--- EnviaAtualizacao/Form1.cs
+++ EnviaAtualizacao/Form1.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-// Ler INI para ver onde esta o arquivo a ser enviado
 // Ler a versão do executável
 // Escrever um arquivo versao.txt com a versão e o texto
 // Ler no INI o caminho a ser enviado
@@ -34,7 +35,11 @@ namespace EnviaAtualizacao
         {
             INI cINI = new INI();
             string Pasta = cINI.ReadString("EnviaAtualizacao", "Pasta", "");
-            int x = 0;
+            string Arq = cINI.ReadString("EnviaAtualizacao", "Arq", "");
+            string Programa = Path.Combine(Pasta, Arq);
+            FileVersionInfo versaoInfo = FileVersionInfo.GetVersionInfo(Programa);
+            string vVersao = versaoInfo.ProductVersion.Substring(0, 5);
+            this.Text = "Praparar versão " + vVersao;
         }
     }
 }
